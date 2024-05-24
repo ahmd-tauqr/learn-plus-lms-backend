119e46d766a8f9457cf26a78c3d247c6c171a297
ahmd-tauqr <ahmd.tauqr@gmail.com>
2024-05-24 22:02:40 +0530

    Initial Commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { CoursesModule } from './courses/courses.module';
+import { LessonsModule } from './lessons/lessons.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [CoursesModule, LessonsModule, UsersModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
